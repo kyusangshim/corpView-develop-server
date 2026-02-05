5e2c4fe7222d4da3fa6f1ff9ffa03378523895ac
kyusangshim <ks030788@naver.com>
2026-02-05 21:42:31 +0900

    fix: #6 Model 폴더 제거

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-# models/__init__.py (브릿지)
-from domains.companies.model import *
-from domains.financials.model import *
-from domains.news.model import *
-from domains.summary.model import *
-from domains.users.model import *
-from domains.industries.model import *
